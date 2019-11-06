575c3fb958f52bd4ef9b4f32ac8d26ad11574244
a.bozhenov <ogoun@users.noreply.github.com>
2019-11-06 16:41:02 +0300

    Fix

--- FileMix/FileMix/FMix.cs
+++ FileMix/FileMix/FMix.cs
@@ -1,15 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Security.Cryptography;
 using System.Threading;
+using System.Threading.Tasks;
 
 namespace FileMix
 {
     public class FMix
     {
-        private static long counter = 0;
         public static string mask = "*.*";
 
+        [MTAThread]
         private static void Main(params string[] args)
         {
             if (args.Length > 0)
@@ -19,6 +21,45 @@ namespace FileMix
                     mask = args[1];
                 }
                 StartMix(args[0]);
+            }
+        }
+
+        static void StartMix(string path)
+        {
+            long counter = 0;
+            var folders = new Stack<string>(Directory.GetDirectories(path));
+            folders.Push(path);
+            using (var sem = new Semaphore(Environment.ProcessorCount, Environment.ProcessorCount))
+            {
+                while (folders.Count > 0)
+                {
+                    var folder = folders.Pop();
+                    Console.WriteLine($"Mix folder {folder}");
+                    foreach (var file in Directory.GetFiles(folder, mask))
+                    {
+                        Interlocked.Increment(ref counter);
+                        sem.WaitOne();
+                        Task.Run(() =>
+                        {
+                            try
+                            {
+                                MixFile(file);
+                            }
+                            catch
+                            {
+                            }
+                            finally
+                            {
+                                sem.Release();
+                                Interlocked.Decrement(ref counter);
+                            }
+                        });
+                    }
+                    foreach (var dir in Directory.GetDirectories(folder))
+                    {
+                        folders.Push(dir);
+                    }
+                }
                 while (counter != 0)
                 {
                     Thread.Sleep(1000);
@@ -26,51 +67,22 @@ namespace FileMix
             }
         }
 
-
-        static void StartMix(string path)
+        static void MixFile(string path)
         {
-            Interlocked.Increment(ref counter);
-
-            var folders = new Stack<string>(Directory.GetDirectories(path));
-            folders.Push(path);
-            while (folders.Count > 0)
+            var b = new byte[8192];
+            var rnd = new RNGCryptoServiceProvider();
+            using (var raf = new FileStream(path, FileMode.Open, FileAccess.Write, FileShare.None))
             {
-                var folder = folders.Pop();
-                Console.WriteLine($"Mix folder {folder}");
-                foreach (var file in Directory.GetFiles(folder, mask))
+                for (var i = 0; i < raf.Length; i += b.Length)
                 {
-                    Interlocked.Increment(ref counter);
-                    ThreadPool.QueueUserWorkItem(MixFile, file);
+                    rnd.GetBytes(b);
+                    var count = (int)((i * b.Length + b.Length) > raf.Length
+                        ? raf.Length - i * b.Length
+                        : b.Length);
+                    raf.Write(b, 0, count);
                 }
-                foreach (var dir in Directory.GetDirectories(folder))
-                {
-                    folders.Push(dir);
-                }
-            }
-            Interlocked.Decrement(ref counter);
-        }
-
-        static void MixFile(object path)
-        {            
-            var rnd = new Random((int)Environment.TickCount);
-            try
-            {
-                var raf = new RandomAccessFile(path.ToString(), "rw");
-                var count = (int)(raf.Length() * .4);
-                var b = new byte[10];
-                var length = raf.Length() - 10;
-                for (var i = 0; i < count; i++)
-                {
-                    rnd.NextBytes(b);
-                    raf.Seek((long)((double)length * rnd.NextDouble()));
-                    raf.Write(b, 0, 10);
-                }
-                raf.Sync();
+                raf.Flush();
                 raf.Close();
-            }
-            finally
-            {
-                Interlocked.Decrement(ref counter);
             }
         }
     }
